ed01612a2da2a95c5194b1c92cde8f6d994fb5cb
bound1ess <bound1ess.dev@gmail.com>
2015-01-03 20:25:31 +0600

    Implement ask_about_options().

--- lib/selectable.rb
+++ lib/selectable.rb
@@ -8,16 +8,31 @@ end
 
 def ask_for_file_path
     print "Where would you like to save selectable.sh? [#{__dir__}]: "
-    input = STDIN.gets.chomp.strip
-    if input.length == 0
-        input = __dir__
+    path = STDIN.gets.chomp.strip
+    if path.length == 0
+        path = __dir__
     end
-    puts "Saving in #{input}", $/
-    input
+    puts "Saving in #{path}", $/
+    path
 end
 
 def ask_about_options
-    # TODO
+    print "Would you like to have menu options predefined or not? [Yes/No]: "
+    choice = STDIN.gets.chomp.strip.downcase[0]
+    if choice == "y"
+        options = []
+        puts "Enter a menu option or just press Enter to quit."
+        until (option = STDIN.gets.chomp.strip).length == 0 do
+            options.push option
+            puts "Added #{option} to the list."
+        end
+        return {:type => "predefined", :list => options}
+    else
+        print "Please enter the path where the options are stored: "
+        path = STDIN.gets.chomp.strip
+        print $/
+        return {:type => "stored", :path => path}
+    end
 end
 
 def ask_about_choice_processing
