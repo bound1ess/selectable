d150cfb96b3608980c23b7a92e851a79e3b200ea
bound1ess <bound1ess.dev@gmail.com>
2015-01-03 19:44:29 +0600

    Add missing functions, add get_bash_shebang_line.

--- lib/selectable.rb
+++ lib/selectable.rb
@@ -1,3 +1,23 @@
 def get_pieces_folder_path
     __dir__ + "/../pieces/"
 end
+
+def get_bash_shebang_line
+    "#!/usr/bin/env bash" + $/
+end
+
+def ask_for_file_path
+    # TODO
+end
+
+def ask_about_options
+    # TODO
+end
+
+def ask_about_choice_processing
+    # TODO
+end
+
+def build_selectable file_path, options, choice_processing
+    # TODO
+end
