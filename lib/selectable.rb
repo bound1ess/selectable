85afe7b5bd83f61b4148ad98148bdf275b363d9c
bound1ess <bound1ess.dev@gmail.com>
2015-01-04 20:25:02 +0600

    Cleanup the code a little bit (to my taste).

--- lib/selectable.rb
+++ lib/selectable.rb
@@ -1,4 +1,4 @@
-def get_pieces_folder_path
+def get_pieces_dir_path
     __dir__ + "/../pieces/"
 end
 
@@ -7,35 +7,43 @@ def get_bash_shebang_line
 end
 
 def get_sh_template name
-    $/ + File.read(get_pieces_folder_path() + "/#{name}.sh")
+    $/ + File.read(get_pieces_dir_path() + "/#{name}.sh")
 end
 
 def ask_for_file_path
     print "Where would you like to save selectable.sh? [#{__dir__}/..]: "
     path = STDIN.gets.chomp.strip
-    if path.length == 0
+
+    if path.empty?
         path = __dir__ + "/.."
     end
+
     puts "Saving in #{path}", $/
-    path
+
+    return path
 end
 
 def ask_about_options
     print "Would you like to have menu options predefined or not? [Yes/No]: "
     choice = STDIN.gets.chomp.strip.downcase[0]
+
     if choice == "y"
         options = []
-        puts "Enter a menu option or just press Enter to quit."
+        puts "Enter a menu option or just press Enter to quit:"
+
         until (option = STDIN.gets.chomp.strip).length == 0 do
             options.push option
             puts "Added #{option} to the list."
         end
+
         puts ("The predefined options are [%s]." % [options.join(", ")]), $/
+
         return {:type => "predefined", :list => options}
     else
         print "Please enter the path where the options are stored: "
         path = STDIN.gets.chomp.strip
         puts "Loading from #{path}", $/
+
         return {:type => "stored", :path => path}
     end
 end
@@ -43,13 +51,16 @@ end
 def ask_about_choice_processing
     print "Would you like to display user's choice OR store it in a variable? [1st/2nd]: "
     choice = STDIN.gets.chomp.strip[0]
+
     if choice == "1"
         puts "You opted to display the user's choice.", $/
+
         return {:type => "show"}
     else
         print "Please enter the variable name: "
         name = STDIN.gets.chomp.strip.upcase
         puts "The user's choice will be stored in #{name}.", $/
+
         return {:type => "store", :variable_name => name}
     end
 end
