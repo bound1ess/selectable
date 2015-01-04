b70d9cc0e0cb018042e7d00f7972fc865bd6f286
bound1ess <bound1ess.dev@gmail.com>
2015-01-04 20:06:15 +0600

    Missing fi was causing troubles.

--- pieces/update-current-index.sh
+++ pieces/update-current-index.sh
@@ -3,6 +3,7 @@ selectable-update-current-index() {
         if (($(($SELECTABLE_CURRENT_INDEX+1)) < $SELECTABLE_OPTIONS_AMOUNT)); then
             SELECTABLE_CURRENT_INDEX=$(($SELECTABLE_CURRENT_INDEX+1))
         fi
+    fi
     if [ "$1" == $SELECTABLE_ARROW_UP ]; then
         if (($(($SELECTABLE_CURRENT_INDEX-1)) >= 0)); then 
             SELECTABLE_CURRENT_INDEX=$(($SELECTABLE_CURRENT_INDEX-1))
